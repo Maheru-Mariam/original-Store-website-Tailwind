3a9d8047d142ce783be13cff2f8404d4458462af
Maheru-Mariam <maherumariam@gmail.com>
2024-07-12 00:24:40 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+
+  mode:"jit",
+  content: ["./*.{html,js}"],
+  theme: {
+   
+    extend: {
+      container: {
+        center: true,
+        Padding: {
+          DEFAULT: '1rem',
+          sm: '1.5 rem',
+        }
+       } ,
+    },
+  },
+  plugins: [],
+}
+
